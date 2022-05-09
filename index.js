ccbe4e348d16aa91131e5d8ce136687042be4690
Tainã Martins Pino <63248706+Tainmat@users.noreply.github.com>
2022-05-09 01:22:27 -0300

    answerandquestions

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ const port = 8080;
 
 /* Atribuindo EJS como View Engine */
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/:nome/:lang', (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
     var showMessage = false;
+
+    var products = [
+        { name: 'Doritos', price: 3.14 },
+        { name: 'Coca-Cola', price: 5.0 },
+        { name: 'Trakinas', price: 2.00}
+    ];
+
     res.render('index.ejs', {
         nome: nome,
         lang: lang,
         message: showMessage,
         company: 'MTCorp',
+        products: products,
     });
 });
 
