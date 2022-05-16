5b92f026b98a5d36335eaffc49b83bcee64a5915
Tainã Martins Pino <63248706+Tainmat@users.noreply.github.com>
2022-05-16 06:44:33 -0300

    body-parser

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const port = 8080;
 
 /* Atribuindo EJS como View Engine */
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('index.ejs');
+/* Body Parser */
+app.use(bodyParser.urlencoded({ extend: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
-app.get('/ask', (req, res) => {
-  res.render('ask.ejs')
-})
+app.get("/ask", (req, res) => {
+  res.render("ask.ejs");
+});
+
+app.post("/saveask", (req, res) => {
+  var tittle = req.body.titulo;
+  var description = req.body.descricao;
+  res.send(`[Tittle]: ${tittle} | [Description]: ${description}`);
+});
 
 app.listen(port, () => {
-    console.log(`[APP IS RUN]: http://localhost:${port}/`);
+  console.log(`[APP IS RUN]: http://localhost:${port}/`);
 });
